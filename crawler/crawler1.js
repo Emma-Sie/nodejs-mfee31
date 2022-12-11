0d0bda993ccd3166c9e910da5751be1ffaf47fc3
Emma-Sie <kktpss93117@gmail.com>
2022-12-11 15:44:55 +0800

    add package-lock.json

--- crawler/crawler1.js
+++ crawler/crawler1.js
@@ -1,13 +1,9 @@
 // 1. 安裝 npm i axios
 // 2. 引用 require
 // 3. 去讀官方文件
-
 const axios = require('axios');
 
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
-
-
-
 axios
   .get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211')
   .then((response) => {
@@ -16,17 +12,4 @@ axios
   .catch((e) => {
     console.error(e);
   });
-
-
-// const axios = require('axios');
-
-// (async () => {
-//     try{
-//         let response = await axios.get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211');   
-//         console.log(response.data); 
-//     }catch(e){
-//         console.error(e);
-//     }
-// })();
-
 
