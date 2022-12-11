dd167f13dc98aac84165218d019acc7e4f0da889
Emma-Sie <kktpss93117@gmail.com>
2022-12-11 15:46:15 +0800

    add crawler2

--- crawler/crawler2.js
+++ crawler/crawler2.js
@@ -5,18 +5,22 @@
 const axios = require('axios');
 
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
-
-
-
-axios
-  .get('http://54.71.133.152:3000/stocks?stockNo=2618&date=202211')
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
+(async () => {
+    try {
+      let stockNo = '2618';
+      let date = '20221111';
+      let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+        params: {
+          stockNo,
+          date,
+        },
+      });
+  
+      console.log('await', response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  })();
 
 // const axios = require('axios');
 
