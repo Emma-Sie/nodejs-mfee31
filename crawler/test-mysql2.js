c7f5cd7cb8db76bc0e07c04f3a35f8210bb6d9d1
Emma-Sie <kktpss93117@gmail.com>
2022-12-17 12:08:48 +0800

    add dotenv test-mysql2.js

--- crawler/test-mysql2.js
+++ crawler/test-mysql2.js
@@ -1,19 +1,22 @@
 
 const mysql2 = require('mysql2/promise');
+require('dotenv').config();
+// let dotenv = require('dotenv');
+// dotenv.config();
 
 (async () => {
   const connection = await mysql2.createConnection({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'admin',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     // 密碼不可推上github，且不留紀錄
-    password: '',
-    database: 'stock_mfee31',
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
 
   // simple query
-  let result = await connection.query('SELECT * FROM `stocks`');
-  let data = result[0];
+  let [data,fields] = await connection.query('SELECT * FROM `stocks`');
+//   let data = result[0];
   // console.log(result);
   console.log(data);
 
